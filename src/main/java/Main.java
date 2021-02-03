19100575c68b21f859c8866eb2d8dee2d1781096
FRANCISCO DE ASSIS GONCALVES NOGUEIRA <8160574@estg.ipp.pt>
2021-02-03 20:50:05 +0000

    add main method for testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+import Exceptions.EmptyCollectionException;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Agent p = new Agent("Kim");
+        try {
+            Simulation s = new Simulation("mission.json", p);
+
+            Iterator i = s.getAutomaticSimulation();
+            while(i.hasNext()){
+                System.out.println(i.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (EmptyCollectionException e) {
+            e.printStackTrace();
+        }
+    }
 }
