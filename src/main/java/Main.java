830754a1ba7d6b4843983e667f96739364854ac7
FRANCISCO DE ASSIS GONCALVES NOGUEIRA <8160574@estg.ipp.pt>
2021-02-04 22:29:07 +0000

    Add menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import Exceptions.EmptyCollectionException;
+import Exceptions.NotComparableException;
 import org.json.simple.parser.ParseException;
 
 
@@ -10,57 +11,164 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        //playMusic("MissionImpossible.wav");
-
-        Agent p = new Agent("Kim");
-        try {
-
-            Simulation s = new Simulation("mission.json", p);
-
-            Iterator i = s.getAutomaticSimulation();
-
-            s.printMatrix();
-
-            s.getManualSimulation();
-            
-            while(i.hasNext()){
-
-                System.out.print(i.next()+" -> ");
-            }
-            System.out.print("Hotel ->");
-            System.out.print(" Trivago ->");
-            System.out.print(" Papar gajas, because it's TÓ fucking CRUZ");
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
+    public static void clearScreen() {
+        for (int i = 0; i < 50; ++i) System.out.println();
+        System.out.flush();
     }
 
-    public static void  playMusic(String filepath){
-
-        try{
-
+    public static void  playMusic(String filepath) {
+        try {
             File musicPath = new File(filepath);
 
-            if(musicPath.exists()){
+            if (musicPath.exists()) {
 
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
-            }else {
+            } else {
 
                 System.out.println("Can't find music");
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
 
             ex.printStackTrace();
         }
     }
-}
+
+    public static void printMenu(Agent p){
+        Scanner s = new Scanner(System.in);
+        String option = "0";
+        while (Integer.parseInt(option)!=4) {
+
+            System.out.println("\u001B[0m _______________________________________");
+            System.out.println("│ Select an option:                     │");
+            System.out.println("│   ─────────────────────────────────   │▒");
+            System.out.println("│  │ 1- Automatic Simulation         │  │▒");
+            System.out.println("│  │ 2- Manual Simulation            │  │▒");
+            System.out.println("│  │ 3- History                      │  │▒");
+            System.out.println("│   ─────────────────────────────────   │▒");
+            System.out.println("│_______________________________________│▒");
+            System.out.println("│ 4- Exit                               │▒");
+            System.out.println("|_______________________________________|▒");
+            System.out.println("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\n");
+
+            System.out.print("Your option: ");
+            option = s.nextLine();
+            clearScreen();
+            try {
+                Simulation simulation = new Simulation("mission.json", p);
+
+                switch (Integer.parseInt(option)) {
+                    case 1:
+                        Iterator i = simulation.getAutomaticSimulation();
+
+                        while (i.hasNext()) {
+                            System.out.print(i.next() + " -> ");
+                        }
+                        break;
+                    case 2:
+                        playMusic("MissionImpossible.wav");
+                        simulation.getManualSimulation();
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        System.out.println("\u001B[32mUsually I never quit, but this is too much\u001B[0m");
+                        return;
+                    default:
+                        System.out.println("\u001B[32mOption " + option + " is not an option, that would kill me\u001B[0m");
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            } catch (NotComparableException e) {
+                e.printStackTrace();
+            } catch (EmptyCollectionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        public static void main(String[] args) {
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("\n" +
+                    "███▓  ███▄ ▄███▓  ██████▒\n" +
+                    "▓██▒ ▓██▒▀█▀ ██▒ ▓██    ▒ \n" +
+                    "▒██▒ ▓██    ▓██░ ▒████  ░ \n" +
+                    "░██░ ▒██    ▒██  ░▓█▒   ░ \n" +
+                    "░██░ ▒██▒   ░██▒ ░▒█░    \n" +
+                    "░▓  ░ ▒░   ░  ░  ▒ ░    \n" +
+                    " ▒  ░░  ░      ░  ░      \n" +
+                    " ▒  ░░      ░     ░ ░    \n" +
+                    " ░         ░           \n");
+
+            System.out.println("The agency Improbable Mission Force wants to know your name.");
+            System.out.print("\u001B[32m I never tell my real name, but you can call me... ");
+            Agent p = new Agent(s.nextLine());
+
+            clearScreen();
+
+            System.out.println("█░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░▰░█");
+            System.out.println("\n" +
+                    "▀█▀ █▀▄▀█ █▀▀█ █▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀▄ █░░ █▀▀ 　 ▒█▀▄▀█ ░▀░ █▀▀ █▀▀ ░▀░ █▀▀█ █▀▀▄ \n" +
+                    "▒█░ █░▀░█ █░░█ █▄▄▀ █░░█ █▀▀▄ █▄▄█ █▀▀▄ █░░ █▀▀ 　 ▒█▒█▒█ ▀█▀ ▀▀█ ▀▀█ ▀█▀ █░░█ █░░█ \n" +
+                    "▄█▄ ▀░░░▀ █▀▀▀ ▀░▀▀ ▀▀▀▀ ▀▀▀░ ▀░░▀ ▀▀▀░ ▀▀▀ ▀▀▀ 　 ▒█░░▒█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░▀ \n" +
+                    "\n" +
+                    "▒█▀▀▀ █▀▀█ █▀▀█ █▀▀ █▀▀ \n" +
+                    "▒█▀▀▀ █░░█ █▄▄▀ █░░ █▀▀ \n" +
+                    "▒█░░░ ▀▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀ \n");
+
+            System.out.println("Welcome aboard \u001B[36m" + p.getName() + "\u001B[32m");
+
+            printMenu(p);
+
+//
+//        try {
+//
+//            Simulation s = new Simulation("mission.json", p);
+//
+//            Iterator i = s.getAutomaticSimulation();
+//
+//            s.printMatrix();
+//
+//            s.getManualSimulation();
+//
+//
+//        } catch (Exception e) {
+//
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void  playMusic(String filepath){
+//
+//        try{
+//
+//            File musicPath = new File(filepath);
+//
+//            if(musicPath.exists()){
+//
+//                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+//                Clip clip = AudioSystem.getClip();
+//                clip.open(audioInput);
+//                clip.start();
+//                clip.loop(Clip.LOOP_CONTINUOUSLY);
+//            }else {
+//
+//                System.out.println("Can't find music");
+//            }
+//        }catch (Exception ex){
+//
+//            ex.printStackTrace();
+//        }
+        }
+    }
